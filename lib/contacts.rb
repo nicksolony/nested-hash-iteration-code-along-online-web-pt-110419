c029b9bfb55f5a5b3be95a91b8e37245daca8da4
Nick Solonyy <nicksolony@gmail.com>
2019-11-19 03:37:47 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -22,7 +22,7 @@ def remove_strawberry(contacts)
         
         data.each do |attribute, value|
           if attribute == :favorite_ice_cream_flavors
-            
+              value.delete_if {|ice_cream| ice_cream == "strawberry"}
       binding.pry    
           end
           
